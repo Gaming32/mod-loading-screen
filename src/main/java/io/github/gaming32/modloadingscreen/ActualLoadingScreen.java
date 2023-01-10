94c07c1236b9df69b3d968705302d334016b953e
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2023-01-09 21:40:06 -0600

    Now that I'm done, I should probably make a Git repo :P
    Still need an icon and README though

---
+++ src/main/java/io/github/gaming32/modloadingscreen/ActualLoadingScreen.java
@@ -0,0 +1,115 @@
+package io.github.gaming32.modloadingscreen;
+
+import com.formdev.flatlaf.FlatDarkLaf;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.entrypoint.EntrypointContainer;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+import java.awt.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+import java.util.*;
+
+public class ActualLoadingScreen {
+    private static final boolean RUNNING_ON_QUILT = FabricLoader.getInstance().isModLoaded("quilt_loader");
+    private static final Set<String> IGNORED_BUILTIN = Set.of(RUNNING_ON_QUILT ? "quilt_loader" : "fabricloader", "java");
+    public static final Set<String> FINAL_ENTRYPOINTS = new HashSet<>(List.of("client", "server", "client_init", "server_init"));
+
+    private static final Map<String, JProgressBar> progressBars = new LinkedHashMap<>();
+    private static JFrame dialog;
+    private static JLabel label;
+
+    public static void startLoadingScreen() throws MalformedURLException {
+        if (GraphicsEnvironment.isHeadless()) {
+            System.out.println("Mod Loading Screen is on a headless environment. Only some logging will be performed.");
+            return;
+        }
+
+        System.out.println("Opening loading screen");
+
+        final String gameNameAndVersion = FabricLoader.getInstance()
+            .getAllMods()
+            .stream()
+            .filter(m -> m.getMetadata().getType().equals("builtin"))
+            .filter(m -> !IGNORED_BUILTIN.contains(m.getMetadata().getId()))
+            .findFirst()
+            .map(m -> m.getMetadata().getName() + ' ' + m.getMetadata().getVersion())
+            .orElse("Unknown Game");
+
+        FlatDarkLaf.setup();
+        UIManager.getDefaults().put("ProgressBar.horizontalSize", new Dimension(146, 18));
+        UIManager.getDefaults().put("ProgressBar.font", UIManager.getFont("ProgressBar.font").deriveFont(18f));
+        UIManager.getDefaults().put("ProgressBar.selectionForeground", new Color(255, 255, 255));
+
+        dialog = new JFrame();
+        dialog.setTitle("Loading " + gameNameAndVersion);
+        dialog.setResizable(false);
+
+        final ImageIcon icon = new ImageIcon(new URL(
+            RUNNING_ON_QUILT
+                ? "https://raw.githubusercontent.com/QuiltMC/art/master/banners/png/quilt-banner.png"
+                : "https://raw.githubusercontent.com/FabricMC/fabricmc.net/main/assets/aof4.png"
+        ));
+        icon.setImage(icon.getImage().getScaledInstance(960, 540, Image.SCALE_SMOOTH));
+        label = new JLabel(icon);
+        final BoxLayout layout = new BoxLayout(label, BoxLayout.Y_AXIS);
+        label.setLayout(layout);
+        label.add(Box.createVerticalGlue());
+        dialog.add(label);
+
+        dialog.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        dialog.pack();
+        dialog.setVisible(true);
+    }
+
+    public static void beforeEntrypointType(String name, Class<?> type) {
+        System.out.println("Preparing loading screen for entrypoint '" + name + "'");
+        if (dialog == null) return;
+
+        final JProgressBar progressBar = new JProgressBar(-1, FabricLoader.getInstance().getEntrypointContainers(name, type).size());
+        progressBar.setStringPainted(true);
+        setLabel(progressBar, name, type, null);
+        progressBars.put(name, progressBar);
+        label.add(progressBar, BorderLayout.SOUTH);
+        dialog.pack();
+    }
+
+    public static void beforeSingleEntrypoint(String typeName, Class<?> typeType, EntrypointContainer<?> entrypoint) {
+        System.out.println("Calling entrypoint container for mod '" + entrypoint.getProvider().getMetadata().getId() + "'");
+        if (dialog == null) return;
+
+        final JProgressBar progressBar = progressBars.get(typeName);
+        progressBar.setValue(progressBar.getValue() + 1);
+        setLabel(progressBar, typeName, typeType, entrypoint.getProvider().getMetadata().getName());
+    }
+
+    public static void afterEntrypointType(String name) {
+        System.out.println("Finished loading screen for entrypoint '" + name + "'");
+        if (dialog == null) return;
+
+        label.remove(progressBars.remove(name));
+        dialog.pack();
+
+        if (
+            FINAL_ENTRYPOINTS.contains(name) // &&
+                // Not doing this because of https://github.com/QuiltMC/quilt-standard-libraries/issues/249
+                // // Hack workaround for differently-named Quilt entrypoints
+                // FabricLoader.getInstance().getEntrypointContainers(name + "_init", Object.class).isEmpty()
+        ) {
+            dialog.dispose();
+            dialog = null;
+        }
+    }
+
+    private static void setLabel(JProgressBar progressBar, String typeName, Class<?> typeType, @Nullable String modName) {
+        final StringBuilder message = new StringBuilder("Loading '").append(typeName)
+            .append("' (").append(typeType.getSimpleName()).append(") \u2014 ")
+            .append(progressBar.getValue()).append('/').append(progressBar.getMaximum());
+        if (modName != null) {
+            message.append(" \u2014 ").append(modName);
+        }
+        progressBar.setString(message.toString());
+    }
+}
---
+++ src/main/java/io/github/gaming32/modloadingscreen/ModLoadingScreen.java
@@ -0,0 +1,159 @@
+package io.github.gaming32.modloadingscreen;
+
+import com.mojang.logging.LogUtils;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.LanguageAdapter;
+import net.fabricmc.loader.api.LanguageAdapterException;
+import net.fabricmc.loader.api.ModContainer;
+import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint;
+import net.lenni0451.reflect.Agents;
+import net.lenni0451.reflect.ClassLoaders;
+import net.lenni0451.reflect.JavaBypass;
+import net.lenni0451.reflect.Methods;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.*;
+import org.slf4j.Logger;
+
+import java.lang.instrument.ClassFileTransformer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.ProtectionDomain;
+import java.util.ListIterator;
+
+public class ModLoadingScreen implements LanguageAdapter {
+    private static final Logger LOGGER = LogUtils.getLogger();
+    private static final boolean RUNNING_ON_QUILT = FabricLoader.getInstance().isModLoaded("quilt_loader");
+
+    private static final String ENTRYPOINT_UTILS = RUNNING_ON_QUILT
+        ? "org/quiltmc/loader/impl/entrypoint/EntrypointUtils"
+        : "net/fabricmc/loader/impl/entrypoint/EntrypointUtils";
+    private static final String ENTRYPOINT_CONTAINER = "net/fabricmc/loader/api/entrypoint/EntrypointContainer";
+    private static final String ENTRYPOINT_CONTAINER_IMPL = "net/fabricmc/loader/impl/entrypoint/EntrypointContainerImpl";
+    private static final String ACTUAL_LOADING_SCREEN = "io/github/gaming32/modloadingscreen/ActualLoadingScreen";
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T> T create(ModContainer mod, String value, Class<T> type) throws LanguageAdapterException {
+        if (type != PreLaunchEntrypoint.class) {
+            throw new LanguageAdapterException("Fake entrypoint only supported on PreLaunchEntrypoint");
+        }
+        return (T)(PreLaunchEntrypoint)() -> {};
+    }
+
+    public static void init() throws Throwable {
+        LOGGER.info("I just want to say... I'm loading " + (RUNNING_ON_QUILT ? "kinda" : "*really*") + " early.");
+
+        final Path tempFlatLaf = Files.createTempFile("FlatLaf", ".jar");
+        tempFlatLaf.toFile().deleteOnExit();
+        ClassLoaders.addToSystemClassPath(
+            FabricLoader.getInstance()
+                .getModContainer("mod-loading-screen")
+                .orElseThrow()
+                .getRootPaths().get(0)
+                .toUri().toURL()
+        );
+        ClassLoaders.addToSystemClassPath(
+            FabricLoader.getInstance()
+                .getModContainer("com_formdev_flatlaf")
+                .orElseThrow()
+                .getRootPaths().get(0)
+                .toUri().toURL()
+        );
+
+        final byte[] aclData = Files.readAllBytes(
+            FabricLoader.getInstance()
+                .getModContainer("mod-loading-screen")
+                .orElseThrow()
+                .findPath(ACTUAL_LOADING_SCREEN + ".bin")
+                .orElseThrow()
+        );
+
+        Methods.invoke(null, Methods.getDeclaredMethod(
+            ClassLoaders.defineClass(ClassLoader.getSystemClassLoader(), ACTUAL_LOADING_SCREEN.replace('/', '.'), aclData),
+            "startLoadingScreen"
+        ));
+
+        Agents.getInstrumentation().addTransformer(new ClassFileTransformer() {
+            @Override
+            public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) {
+                return className.equals(ENTRYPOINT_UTILS) ? instrument(classfileBuffer) : null;
+            }
+        }, true);
+        Agents.getInstrumentation().retransformClasses(Class.forName(ENTRYPOINT_UTILS.replace('/', '.')));
+    }
+
+    private static byte[] instrument(byte[] bytes) {
+        final ClassNode clazz = new ClassNode();
+        new ClassReader(bytes).accept(clazz, 0);
+
+        final MethodNode method = clazz.methods.stream()
+            .filter(m -> m.name.equals("invoke0"))
+            .findFirst()
+            .orElseThrow();
+        final ListIterator<AbstractInsnNode> it = method.instructions.iterator();
+
+        it.add(new VarInsnNode(Opcodes.ALOAD, 0));
+        it.add(new VarInsnNode(Opcodes.ALOAD, 1));
+        it.add(new MethodInsnNode(
+            Opcodes.INVOKESTATIC,
+            ACTUAL_LOADING_SCREEN, "beforeEntrypointType",
+            "(Ljava/lang/String;Ljava/lang/Class;)V",
+            false
+        ));
+
+        final int container = RUNNING_ON_QUILT ? 7 : 6;
+        while (it.hasNext()) {
+            if (!(it.next() instanceof VarInsnNode varInsn)) continue;
+            if (varInsn.getOpcode() == Opcodes.ASTORE && varInsn.var == container) break;
+        }
+        it.add(new VarInsnNode(Opcodes.ALOAD, 0));
+        it.add(new VarInsnNode(Opcodes.ALOAD, 1));
+        if (RUNNING_ON_QUILT) {
+            it.add(new TypeInsnNode(Opcodes.NEW, ENTRYPOINT_CONTAINER_IMPL));
+            it.add(new InsnNode(Opcodes.DUP));
+        }
+        it.add(new VarInsnNode(Opcodes.ALOAD, container));
+        if (RUNNING_ON_QUILT) {
+            it.add(new MethodInsnNode(
+                Opcodes.INVOKESPECIAL,
+                ENTRYPOINT_CONTAINER_IMPL, "<init>",
+                "(Lorg/quiltmc/loader/api/entrypoint/EntrypointContainer;)V",
+                false
+            ));
+        }
+        it.add(new MethodInsnNode(
+            Opcodes.INVOKESTATIC,
+            ACTUAL_LOADING_SCREEN, "beforeSingleEntrypoint",
+            "(Ljava/lang/String;Ljava/lang/Class;L" + ENTRYPOINT_CONTAINER + ";)V",
+            false
+        ));
+
+        while (it.hasNext()) {
+            if (!(it.next() instanceof InsnNode insn)) continue;
+            if (insn.getOpcode() == Opcodes.IFNULL) break;
+        }
+        it.previous();
+        it.previous();
+        it.add(new VarInsnNode(Opcodes.ALOAD, 0));
+        it.add(new MethodInsnNode(
+            Opcodes.INVOKESTATIC,
+            ACTUAL_LOADING_SCREEN, "afterEntrypointType",
+            "(Ljava/lang/String;)V",
+            false
+        ));
+
+        final ClassWriter writer = new ClassWriter(ClassWriter.COMPUTE_MAXS | ClassWriter.COMPUTE_FRAMES);
+        clazz.accept(writer);
+        return writer.toByteArray();
+    }
+
+    static {
+        try {
+            init();
+        } catch (Throwable t) {
+            JavaBypass.getUnsafe().throwException(t);
+        }
+    }
+}
